12119b8915d088520065c2913290166385fc19cf
Goutham Josh 么 <97418751+GouthamSER@users.noreply.github.com>
2026-03-08 20:28:17 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,9 +1,9 @@
 from pyrogram import Client, filters
-from info import CHANNELS
-from database.ia_filterdb import save_file2, save_file3, save_file4, save_file5, check_file
+from info import CHANNELS, DATABASE_URI, DATABASE_URI2, DATABASE_URI3, DATABASE_URI4, DATABASE_URI5
+from database.ia_filterdb import save_file, check_file, choose_mediaDB
+from sample_info import tempDict
 
 media_filter = filters.document | filters.video | filters.audio
-
 
 @Client.on_message(filters.chat(CHANNELS) & media_filter)
 async def media(bot, message):
@@ -18,27 +18,16 @@ async def media(bot, message):
     media.file_type = file_type
     media.caption = message.caption
     
-    if message.id % 4 == 0:
-        tru = await check_file(media)
-        if tru == "okda":
-            await save_file2(media)
-        else:
-            print("skipped duplicate file from saving to db 😌")
-    elif message.id % 4 == 1:
-        tru = await check_file(media)
-        if tru == "okda":
-            await save_file3(media)
-        else:
-            print("skipped duplicate file from saving to db 😌")
-    elif message.id % 4 == 2:
-        tru = await check_file(media)
-        if tru == "okda":
-            await save_file4(media)
-        else:
-            print("skipped duplicate file from saving to db 😌")
+    tru = await check_file(media)
+    if tru == "okda":
+        # Put all 5 URIs in a list
+        db_uris = [DATABASE_URI, DATABASE_URI2, DATABASE_URI3, DATABASE_URI4, DATABASE_URI5]
+        
+        # Distribute the files evenly across the 5 databases using the message ID
+        tempDict['indexDB'] = db_uris[message.id % 5]
+        await choose_mediaDB() # Force the db filter to recognize the change
+        
+        # Save the file to the chosen database
+        await save_file(media)
     else:
-        tru = await check_file(media)
-        if tru == "okda":
-            await save_file5(media)
-        else:
-            print("skipped duplicate file from saving to db 😌")
+        print("skipped duplicate file from saving to db 😌")
